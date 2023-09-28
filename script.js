3149cbf3cbfacc401d2c1b7de4fe6595748cd109
Yohannes Mulu <dev.yohannes@gmail.com>
2023-09-28 14:25:14 +0300

    removing the outer event listener for the inputs

--- script.js
+++ script.js
@@ -14,118 +14,84 @@ const submitButton = document.querySelector("#submit");
 errorText = document.createElement("p");
 errorText.classList.add("error-message");
 
-/*
-const removeBorderAddErrorBorder = () => {
-  usernameInput.classList.remove("border-class");
-  usernameInput.classList.add("error");
-
-  emailInput.classList.remove("border-class");
-  emailInput.classList.add("error");
-
-  passwordInput.classList.remove("border-class");
-  passwordInput.classList.add("error");
-
-  confirmPasswordInput.classList.remove("border-class");
-  confirmPasswordInput.classList.add("error");
-};
-
-const removeErrorAddSuccess = () => {
-  usernameInput.classList.remove("error");
-  usernameInput.classList.add("success");
-
-  emailInput.classList.remove("error");
-  emailInput.classList.add("success");
-
-  passwordInput.classList.remove("error");
-  passwordInput.classList.add("success");
-
-  confirmPasswordInput.classList.remove("error");
-  confirmPasswordInput.classList.add("success");
-};
-*/
-
 // validations
 const usernameValidator = /^[a-zA-Z]{3,}$/;
 const emailValidator = /[@.]/;
 const passwordValidator = /[a-zA-Z0-9]{5,}/;
 
-// username validation
-usernameInput.addEventListener("keyup", () => {
-  if (usernameValidator.test(usernameInput.value)) {
-    usernameInput.classList.remove("border-class");
-    usernameInput.classList.add("success");
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //
+  // username validation
+  const usernameKeyup = usernameInput.addEventListener("keyup", () => {
+    if (usernameValidator.test(usernameInput.value)) {
+      usernameInput.classList.remove("border-class");
+      usernameInput.classList.add("success");
+    } else {
+      usernameInput.classList.remove("border-class");
+      usernameInput.classList.remove("success");
+      usernameInput.classList.add("error");
+    }
+  });
+
+  // email validation
+  const emailKeyup = emailInput.addEventListener("keyup", () => {
+    if (emailValidator.test(emailInput.value)) {
+      emailInput.classList.remove("border-class");
+      emailInput.classList.add("success");
+    } else {
+      emailInput.classList.remove("border-class");
+      emailInput.classList.add("error");
+    }
+  });
+
+  // password validator
+  const passwordKeyup = passwordInput.addEventListener("keyup", () => {
+    if (passwordValidator.test(passwordInput.value)) {
+      localStorage.setItem("savedPassword", passwordInput.value);
+      passwordInput.classList.remove("border-class");
+      passwordInput.classList.add("success");
+    } else {
+      passwordInput.classList.remove("border-class");
+      passwordInput.classList.add("error");
+    }
+  });
+
+  const confirmPasswordKeyup = confirmPasswordInput.addEventListener(
+    "keyup",
+    () => {
+      if (
+        confirmPasswordInput.value === localStorage.getItem("savedPassword")
+      ) {
+        confirmPasswordInput.classList.remove("border-class");
+        confirmPasswordInput.classList.add("success");
+      } else {
+        confirmPasswordInput.classList.remove("border-class");
+        confirmPasswordInput.classList.add("error");
+      }
+    }
+  );
+  //
+
+  if ((usernameKeyup, emailKeyup, passwordKeyup, confirmPasswordKeyup)) {
+    alert("Thank you for registering");
+    usernameInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
+    confirmPasswordInput.value = "";
   } else {
     usernameInput.classList.remove("border-class");
     usernameInput.classList.remove("success");
     usernameInput.classList.add("error");
-  }
-});
 
-// email validation
-emailInput.addEventListener("keyup", () => {
-  if (emailValidator.test(emailInput.value)) {
-    emailInput.classList.remove("border-class");
-    emailInput.classList.add("success");
-  } else {
     emailInput.classList.remove("border-class");
     emailInput.classList.add("error");
-  }
-});
 
-// password validator
-passwordInput.addEventListener("keyup", () => {
-  if (passwordValidator.test(passwordInput.value)) {
-    localStorage.setItem("savedPassword", passwordInput.value);
-    passwordInput.classList.remove("border-class");
-    passwordInput.classList.add("success");
-  } else {
     passwordInput.classList.remove("border-class");
     passwordInput.classList.add("error");
-  }
-});
 
-confirmPasswordInput.addEventListener("keyup", () => {
-  if (confirmPasswordInput.value === localStorage.getItem("savedPassword")) {
-    confirmPasswordInput.classList.remove("border-class");
-    confirmPasswordInput.classList.add("success");
-  } else {
     confirmPasswordInput.classList.remove("border-class");
     confirmPasswordInput.classList.add("error");
   }
-});
-
-/*
-const divs = [
-  usernameDiv.div,
-  emailDiv.div,
-  passwordDiv.div,
-  confirmPasswordDiv.div,
-];
-*/
-
-submitButton.addEventListener("mouseenter", () => {
-  usernameInput.classList.add("border-class");
-  usernameInput.classList.remove("success");
-  usernameInput.classList.remove("error");
-
-  emailInput.classList.add("border-class");
-  emailInput.classList.remove("success");
-  emailInput.classList.remove("error");
-
-  passwordInput.classList.add("border-class");
-  passwordInput.classList.remove("success");
-  passwordInput.classList.remove("error");
-
-  confirmPasswordInput.classList.add("border-class");
-  confirmPasswordInput.classList.remove("success");
-  confirmPasswordInput.classList.remove("error");
-});
-
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  alert("Thank you for registering");
-  usernameInput.value = "";
-  emailInput.value = "";
-  passwordInput.value = "";
-  confirmPasswordInput.value = "";
 });
